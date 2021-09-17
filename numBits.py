e97436cebcaf1c22ef930ec0e6a9c77a56fdfc0d
Austen Sorochak <austensorochak@gmail.com>
2021-09-17 12:22:34 -0700

    corrected numBits

---
+++ numBits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# Write a program that prompts for a number and then prints out the number of bits in that number. 
+# For example, 10 has 2 bits in it. 
+# Assume the number is valid.
+
+
+num = int(input('Enter and integer\n')) # Assume a valid number
+count = 0
+
+while(num):
+    count += num & 1
+    num >>= 1
+print(count)
+
